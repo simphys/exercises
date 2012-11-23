1cacd40e879cc018d7c908ea25612ca48fca1175
Sebastian <weber.se@gmail.com>
2012-11-23 20:19:27 +0100

    S: Causing a merge on purpose, Part 6

--- SimPhys_W2/X_template.py
+++ SimPhys_W2/X_template.py
@@ -19,6 +19,6 @@ p = Plotter(show = True, save = True, pgf = False, name=filename, directory = ''
 # ==== CALCULATION ====
 
 # ==== PLOTTING ====
-p.new(title=u'Titlename')
+p.new(title=u'Title')
 p.plot(np.linspace(0,10,100), np.linspace(0,10,100)**2, '+-', label=u'Label % *.2f' % (6,0.42))
 p.make()
