5849e29a7a016a4f6938f7ad9b798cae09aba589
April Cooper <april.mae.cooper@gmail.com>
2013-01-22 20:38:07 +0100

    Report bis ausschliesslich Section 4 Ising-Modell

--- SimPhys_W5/Simple_sampling.py
+++ SimPhys_W5/Simple_sampling.py
@@ -3,24 +3,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
+## Functions
+## Calculating f(x)
 def runge(x):
     return 1/(1+x**2)
 
+## Exact integral of f(x) 
 def runge_int_ex(a,b):
     return math.atan(b)-math.atan(a)
 
+## Simple sampling
 def simple_sampling(f,a,b,N):
     xi=np.random.uniform(a,b,N)
     return (b-a)*np.sum(f(xi))/N, (b-a)*np.std(f(xi))/math.sqrt(N)
 
-
+## Plot of the runge function
 #x=np.linspace(-5,5,1000)
 #y=runge(x)
 #plt.plot(x,y)
+#plt.xlabel('x')
+#plt.ylabel('f(x)')
+#plt.xlim([-5,5])
+#plt.ylim([0,1.1])
 #plt.show()
+
 
 exact_solution=runge_int_ex(-5,5)
 print exact_solution
+
+## Task on simple sampling
 
 #print simple_sampling(runge,-5,5,1000)
 
@@ -33,5 +44,7 @@ act_error=np.abs(erg_I[:,0]-exact_solution)
 N=2**np.arange(2,21)
 plt.semilogx(N,act_error,label="actual error")
 plt.semilogx(N,erg_I[:,1],label="statistical error")
+plt.xlabel('Number of integrationsteps')
+plt.ylabel('Error')
 plt.legend()
 plt.show()
