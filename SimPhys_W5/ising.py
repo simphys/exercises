922d532ce7e48c53aef108cfa8826544861fe15a
Sebastian <weber.se@gmail.com>
2013-01-23 16:55:49 +0100

    S: Berechnungen beschleunigt

--- SimPhys_W5/ising.py
+++ SimPhys_W5/ising.py
@@ -26,7 +26,7 @@ itmp = np.arange(-1,n-1)
 #==Exact===
 def generateAllStates(n=n):
     nsqrt = n*n
-    possibilities = np.mgrid[[slice(-1,3,2) for _ in range(nsqrt)]] # Sorry for the hard to read and memory consuming vectorization, however it makes the calculation fast as hell.
+    possibilities = np.mgrid[[slice(-1,3,2) for _ in range(nsqrt)]] #Vectorization makes the calculation fast as hell.
     configurations = possibilities.reshape(n,n,-1).T 
     return configurations
 
@@ -46,46 +46,45 @@ def calcMean(O,E,T):
     return Mean
 
 #==MC===
-def calcE(configuration, J=J, H=H, n=n, i = itmp):
-    E = -J*np.sum(configuration*configuration[:,i]+configuration*configuration[i,:])/n**2
-    E -= H*np.sum(configuration)/n**2
-    return E
-
-def calcM(configuration, n=n):
-    M = np.sum(configuration)/n**2
-    return M
-
-def trialFlip(Conf, n=n):
-    iRandom = np.random.randint(0,n,2)
-    newConf = Conf.copy()
-    newConf[iRandom[0],iRandom[1]] *= -1
-    return newConf
-
-def metropolisMC(N,trialMove,Phi,T):
-    actrate=0
-    E = calcE(Phi)
-    P = np.exp(-E/T)
-    M = calcM(Phi)
+def metropolisMC(N,Phi,T, n=n, i=itmp):
+    # Calculate start values
+    E = (-J*np.sum(configuration*configuration[:,i]+configuration*configuration[i,:])-H*np.sum(configuration))/n**2 # Energy
+    M = np.sum(Phi)/n**2 # Magnetization
+    P = np.exp(-E/T) # Probability
     
+    # Initialize variables
     arrayE = np.empty(N+1)
     arrayM = np.empty(N+1)
     arrayP = np.empty(N+1)
     arrayE[0] = E
     arrayM[0] = M
     arrayP[0] = P
+    actrate=0
     
     for i in range(1,N+1):
-        newPhi = trialMove(Phi)
-        newE = calcE(newPhi) # ToDo: Nur Änderung neu berechnen, es mit zwei Änderungen versuchen
-        newP = np.exp(-newE/T)
-        r = np.random.uniform(0,1)
+        # Select a spin
+        k, l = np.random.randint(0,n,2)
+        k_p = k+1
+        l_p = l+1
+        if k_p >= n: k_p = 0
+        if l_p >= n: l_p = 0
         
+        # Performe a trial move
+        newE = E+2*Phi[k,l]*(J*(Phi[k-1,l]+Phi[k_p,l]+Phi[k,l-1]+Phi[k,l_p])+H)/n**2
+        newP = np.exp(-newE/T)
+        
+        # Attempt to accept the move
+        r = np.random.uniform(0,1)
         if r < min(1,newP/P):
-            Phi = newPhi
+            
+            # If move accepted, save new values
             E = newE
-            M = calcM(Phi)
             P = newP
+            M = M-2*Phi[k,l]/n**2
+            Phi[k,l] *= -1
             actrate+=1
+
+        # Write current values in array
         arrayE[i] = E
         arrayM[i] = M
         arrayP[i] = P
@@ -110,6 +109,8 @@ for i in range(len(T)):
     meanE[i] = calcMean(E,E,T[i])
     meanM[i] = calcMean(M,E,T[i])
 
+print 'Finished exact calculation.'
+
 #==MC===
 configuration = 2*np.random.randint(0,2,(n,n))-np.ones((n,n))
 
@@ -119,8 +120,7 @@ arrayP = np.empty((len(T),MCSteps+1))
 arrayA = np.empty(len(T))
 
 for i in range(len(T)):
-    arrayE[i], arrayM[i], arrayP[i], arrayA[i] = metropolisMC(MCSteps,trialFlip,configuration,T[i])
-    print T[i]
+    arrayE[i], arrayM[i], arrayP[i], arrayA[i] = metropolisMC(MCSteps,configuration,T[i])
 
 MC_meanE = np.mean(arrayE,axis=1)
 MC_meanM = np.mean(arrayM,axis=1)
@@ -130,12 +130,13 @@ MC_E = arrayE.flat
 MC_M = arrayM.flat
 MC_P = arrayP
 
+print 'Finished metropolis calculation.'
+
 '''
 === PLOTS ===
 '''
 p = Plotter(show = True, pdf = False, pgf = False, name='ising')
 
-#==Exact===
 p.new(name='Mean energy',xlabel='Temperature',ylabel='Energy')
 p.plot(T,meanE,label='exact')
 p.plot(T,MC_meanE,label='metropolis')
@@ -152,4 +153,5 @@ time = (T*np.ones_like(arrayP).T).T
 H, xedges, yedges = np.histogram2d(time.flatten(), MC_P.flatten(), bins=(len(T),100))
 p.imshow(H, extent=[yedges[0], yedges[-1], xedges[0], xedges[-1]], interpolation='nearest',aspect='auto',origin='lower')
 
+print 'Finished plots.'
 p.make(ncols=2)
