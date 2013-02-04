4c347f17097168e140ef8f9bf2887e4a5366738b
Patrick Kreissl <patrick@Nachtigaller.(none)>
2013-02-04 15:38:40 +0100

    P: added task 4.1 and 4.2

--- SimPhys_W6/ising.py
+++ SimPhys_W6/ising.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python2
 # -*- coding:utf-8 -*-
 # Author: Sebastian Weber, 5 PI Universitaet Stuttgart
+# some additions by: Patrick Kreissl, ICP Universitaet Stuttgart
 
 from __future__ import division
 from libs.simlib import Plotter, Params
@@ -20,6 +21,9 @@ params.Ising_H = 0
 params.MC_Seed = 42
 params.MC_Sweeps = 10000
 
+#chose between cases 1, 2 and 3 and comment the others out
+
+# standard parameters (used for tasks 2, 3)
 params.T_Start = 1
 params.T_Stop = 5
 params.T_StepSize = 0.1
@@ -27,9 +31,35 @@ params.T_StepSize = 0.1
 Ising_L = [4,16,64]
 Binning_K = [50,200,800]
 
+Ising_L = [4,16,64]
+Binning_K = [50,200,800]
+
+# Choose the right parameters according to task
+
+# standard parameters
+OverParam = 0
+
+if OverParam != 0:
+    # parameters to determine T_c and calculate the Binder parameter
+    if OverParam == 1:
+        params.T_Start = 2
+        params.T_Stop = 2.4
+        params.T_StepSize = 0.005 # only 0.02 would be needed
+
+        Ising_L = [4,16,64]
+        Binning_K = [50,200,800]
+    # parameters used to estimate beta_m
+    elif OverParam == 2:
+        params.T_Start = 2.27 # << estimated critical Temperature
+        params.T_Stop = params.T_Start
+        params.T_StepSize = 0.01 # only 0.02 would be needed
+
+        Ising_L = [4,16,32,64,128]
+        Binning_K = [50,200,800,800,800]
+
 useCache = True
 
-p = Plotter(show = True, pdf = False, pgf = False, latex=False, name='ising')
+p = Plotter(show = False, pdf = True, pgf = False, latex=False, name='ising')
 
 # FUNCTIONS ###############################################################################################
 #==Exact===
@@ -73,6 +103,15 @@ def errorAll(func,arr,k):
     result = np.empty(n)
     for i in range(n): result[i] = func(arr[i],k)
     return result
+
+#===Finite Size Scaling===
+
+def binderpar(mu):
+    n = np.shape(mu)[0]
+    result = np.empty(n)   
+    for i in range(n): result[i] = 1 - 1./3 * np.mean(mu[i,:]**4)/np.mean(mu[i,:]**2)**2
+    return result
+
 
 #===Others===
 def load(Ising_L):
@@ -133,6 +172,7 @@ E = []
 M = []
 errE = []
 errM = []
+U = []
 
 for i in range(len(Ising_L)):
     arr = load(Ising_L[i])
@@ -145,6 +185,7 @@ for i in range(len(Ising_L)):
     M.append(np.mean(arrM[i],axis=1))
     errE.append(errorAll(binning,arrE[i],Binning_K[i]))
     errM.append(errorAll(binning,arrM[i],Binning_K[i]))
+    U.append(binderpar(arrM[i]))
 
 print 'Finished calculations.'
 
@@ -158,7 +199,7 @@ for i in range(len(Ising_L)): p.errorbar(T[i], E[i], yerr=errE[i], label='L=%s, 
 p.new(title='Mean absolute magnetization',xlabel='Temperature',ylabel=r'$\vert Magnetization \vert$')
 p.plot(T_exact,M_exact,label='for L = 4, exact')
 for i in range(len(Ising_L)): p.errorbar(T[i], M[i], yerr=errM[i], label='L=%s, MC'%Ising_L[i])
-p.make(ncols=1,show=False)
+#p.make(ncols=1,show=False)
 
 for i in range(len(Ising_L)):
     p.new(title='Frequency of energies (L=%s)'%Ising_L[i],xlabel='Energy',ylabel='Temperature')
@@ -170,7 +211,7 @@ for i in range(len(Ising_L)):
     temp = (T[i]*np.ones_like(arrM[i]).T).T
     H, xedges, yedges = np.histogram2d(temp.flatten(), arrM[i].flatten(), bins=(len(T[i]),100))
     p.imshow(H, extent=[yedges[0], yedges[-1], xedges[0], xedges[-1]], interpolation='nearest',aspect='auto',origin='lower',norm=LogNorm())
-p.make(ncols=2,show=False)
+#    p.make(ncols=2,show=False)
 
 for i in range(len(Ising_L)):
     p.new(title='Binning error (L=%s)'%Ising_L[i],xlabel='k',ylabel='error')
@@ -183,6 +224,18 @@ for i in range(len(Ising_L)):
     p.new(title='Energy of absolute magnetization (L=%s)'%Ising_L[i],xlabel='Absolute magnetization',ylabel='Energy')
     [X, Y] = unique(np.array([np.round(arrM[i].ravel(),2),np.round(arrE[i].ravel(),2)]).T).T
     p.plot(X,Y,'o',alpha=0.1)
+
+p.new(title='Binder parameter',xlabel='Temperature',ylabel='Binder parameter')
+n = np.shape(T)[0]
+for i in range(n): p.plot(T[i],U[i],label='for L = %s'%i)
+
+p.new(title='Binder parameter',xlabel='1/Temperature = beta*k_B',ylabel='Binder parameter')
+n = np.shape(T)[0]
+for i in range(n): p.plot(1./T[i],U[i],label='for L = %s'%i)
+
+p.new(title='magnetization',xlabel='L',ylabel='magnetization M',xscale='log',yscale='log')
+p.plot(Ising_L,M)
+
 p.make(ncols=2,show=True)
 
 print 'Finished plots.'
